17c9624fe6cb0761cfbb7a53107829e19d8ada7a
Simon Garber <Simon.garber@gmail.com>
2019-07-17 09:40:33 -0400

    did some styling changes the html and css pages.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,65 +4,60 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const createTweetElement = function(tweet) {
+    let $tweet = $('<article>').addClass(`tweet`)
+    $tweet.append($('<img>').attr(`src`,tweet.user.avatars))
+    $tweet.append($('<h4>').text(tweet.user.name))
+    $tweet.append($('<h3>').text(tweet.user.handle))
+    $tweet.append($('<p>').text( tweet.content.text))
+    $tweet.append($('<span>').addClass('date_created').text(tweet.created_at))
+    return $tweet
+   }
 
+   
 
-const tweets = [
+   const renderTweets = function(tweets) {
+    for( let i in tweets) {
+     $('.tweets').append(createTweetElement(tweets[i]))
+    } 
+ }
+
+ function loadTweets()    
     {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
+        $.ajax({
+        url:'/tweets',
+        method: 'GET',
+        success: function(res) {
+          $('tweets').empty()
+          renderTweets(res);
+        } 
+      });  
     }
-  ]
 
-  const renderTweets = function(tweets) {
-   for( let i in tweets) {
-    $('.tweets').append(createTweetElement(tweets[i]))
-   } 
-}
-
-const createTweetElement = function(tweetData) {
- let $tweet = $('<article>').addClass(`tweet`)
- $tweet.append($('<img>').attr(`src`,tweetData.user.avatars))
- $tweet.append($('<h4>').text(tweetData.user.name))
- $tweet.append($('<h3>').text(tweetData.user.handle))
- $tweet.append($('<p>').text( tweetData.content.text))
- $tweet.append($('<span>').addClass('date_created').text(tweetData.created_at))
- return $tweet
-}
- 
 $(document).ready(function() {
- console.log("Document is ready");
-// renderTweets(tweets)
+    console.log("Document is ready");
+   
+    $("#submit_tweet").on("submit",(function(event) {
+        event.preventDefault();
+        console.log("Default event prevented")
+        $.ajax({
+                url:'/tweets',
+                method:'POST',
+                data: $(this).serialize(), 
+                success: () => console.log("Success!")   
+              });
+    }));
 
-$("#submit_tweet").on("submit",(function(event) {
-    event.preventDefault();
-    console.log("Default event prevented")
-    $.ajax({
-            url:'/tweets',
-            method:'POST',
-            data: $(this).serialize(), 
-            success: () => console.log("Success!")   
-          });
-}));
-renderTweets(tweets)
-});
+    
+      loadTweets();
+    });
+
+
+ 
+
+
+
+
 
 
 
